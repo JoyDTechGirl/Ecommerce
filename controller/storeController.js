e3ef1f0f02073140d7ed5134e553a84f9e9c3725
JoyDTechGirl <samsonjoyuwaoma7@gmail.com>
2025-01-24 18:06:56 +0100

    i now made new change

--- controller/storeController.js
+++ controller/storeController.js
@@ -73,7 +73,7 @@ exports.deleteStore = async(req,res) =>{
       return res.status(404).json('store not found')
     }
 
-    // store.destory()
+     store.delete()
     res.status(200).json('store deleted')
   }catch(err){
     res.status(500).json({error: err.message})
